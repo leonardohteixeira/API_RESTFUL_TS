dcf02bf0fb7ec1556f52043923368ab497987fce
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-03-11 18:46:43 -0300

    Adicionado função para atualizar dados dos filme

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { createMovie, findMovieById, getAllMovies, removeMovie } from "./controllers/movieControllers";
+import { createMovie, findMovieById, getAllMovies, removeMovie, updateMovie } from "./controllers/movieControllers";
 
 //validations
 import { validate } from "./middleware/handleValidation";
@@ -12,4 +12,5 @@ export default router.get("/test", (req: Request, res: Response) => {
 }).post("/movie", movieCreateValidation(), validate, createMovie)
 .get("/movie/:id", findMovieById)
 .get("/movie", getAllMovies)
-.delete("/movie/:id", removeMovie);
+.delete("/movie/:id", removeMovie)
+.patch("/movie/:id", movieCreateValidation(), validate, updateMovie);
