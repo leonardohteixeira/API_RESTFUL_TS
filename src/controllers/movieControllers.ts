6b80357e69e5e43680c9840795509e0107295ac1
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-03-11 18:19:33 -0300

    Criado função pra pesquisar os filmes por ID

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -16,3 +16,21 @@ export async function createMovie(req: Request, res: Response) {
     }
 }
 
+export async function findMovieById(req: Request, res: Response) {
+    try{
+        
+        const id = req.params.id
+        const movie = await MovieModel.findById(id);
+
+        if(!movie){
+            return res.status(404).json({ error: "O filme não existe."});
+        }
+
+        return res.status(200).json(movie);
+
+    } catch (e: any) {
+        Logger.info(`Erro no sistema: ${e.message}`);
+    }
+
+}
+
