4da964560010d8ba8dfcaa696396ba3dfb3d4c85
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-03-11 18:39:01 -0300

    Adicionado função para remover filmes

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -1,47 +1,76 @@
 import { Request, Response } from "express";
 
-//Model
+// Model
 import { MovieModel } from "../models/Movie";
 
-//Logger
+// Logger
 import Logger from "../../config/logger";
 
 export async function createMovie(req: Request, res: Response) {
-    try {
-        const data = req.body
-        const movie = await MovieModel.create(data)
-        return res.status(201).json(movie)
-    } catch (e: any) {
-        Logger.info(`Erro no sistema: ${e.message}`);
-    }
+  try {
+    const data = req.body;
+    const movie = await MovieModel.create(data);
+    return res.status(201).json(movie);
+  } catch (e: any) {
+    Logger.info(`Erro no sistema: ${e.message}`);
+  }
 }
 
 export async function findMovieById(req: Request, res: Response) {
-    try{
-        
-        const id = req.params.id
-        const movie = await MovieModel.findById(id);
+  try {
+    const id = req.params.id;
+    const movie = await MovieModel.findById(id);
 
-        if(!movie){
-            return res.status(404).json({ error: "O filme não existe."});
-        }
-
-        return res.status(200).json(movie);
-
-    } catch (e: any) {
-        Logger.info(`Erro no sistema: ${e.message}`);
+    if (!movie) {
+      return res.status(404).json({ error: "O filme não existe." });
     }
 
+    return res.status(200).json(movie);
+  } catch (e: any) {
+    Logger.info(`Erro no sistema: ${e.message}`);
+  }
 }
 
-export async function getAllMovies(req: Request, res:Response){
-    try{
+export async function getAllMovies(req: Request, res: Response) {
+  try {
+    const movies = await MovieModel.find();
+    return res.status(200).json(movies);
+  } catch (e: any) {
+    Logger.info(`Erro no sistema: ${e.message}`);
+  }
+}
 
-        const movies = await MovieModel.find();
+export async function removeMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const movie = await MovieModel.findById(id);
 
-        return res.status(200).json(movies);
-        
-    } catch (e: any) {
-        Logger.info(`Erro no sistema: ${e.message}`);
+    if (!movie) {
+      return res.status(404).json({ error: "O filme não existe." });
     }
+
+    await movie.deleteOne();
+
+    return res.status(201).json({ msg: "Filme removido com sucesso!" });
+  } catch (e: any) {
+    Logger.info(`Erro no sistema: ${e.message}`);
+  }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const data = req.body
+    const movie = await MovieModel.findById(id);
+
+    if (!movie) {
+      return res.status(404).json({ error: "O filme não existe." });
+    }
+
+    await MovieModel.updateOne({_id: id}, data);
+
+    return res.status(200).json(data);
+  } catch (e: any) {
+    Logger.info(`Erro no sistema: ${e.message}`);
+  }
 }
