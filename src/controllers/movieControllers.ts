fbc35d9f1bf46156ffbd6b223d65115cdc4b4663
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-03-11 18:23:35 -0300

    Adicionado função para pesquisar todos os filmes

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -34,3 +34,14 @@ export async function findMovieById(req: Request, res: Response) {
 
 }
 
+export async function getAllMovies(req: Request, res:Response){
+    try{
+
+        const movies = await MovieModel.find();
+
+        return res.status(200).json(movies);
+        
+    } catch (e: any) {
+        Logger.info(`Erro no sistema: ${e.message}`);
+    }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { createMovie, findMovieById } from "./controllers/movieControllers";
+import { createMovie, findMovieById, getAllMovies } from "./controllers/movieControllers";
 
 //validations
 import { validate } from "./middleware/handleValidation";
@@ -10,4 +10,5 @@ const router = Router();
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send("API Working!");
 }).post("/movie", movieCreateValidation(), validate, createMovie)
-.get("/movie/:id", findMovieById);
+.get("/movie/:id", findMovieById)
+.get("/movie", getAllMovies);
